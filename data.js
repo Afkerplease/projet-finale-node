5ce6fdf20473e9d2201723784d2f99c955d589b6
afker please <afker2020@gmail.com>
2024-03-08 11:10:02 +0100

    creationg ejs+ get user by id

---
+++ data.js
@@ -0,0 +1,43 @@
+const utilisateurs = [
+  {
+    id: 1,
+    first_name: "Shandee",
+    last_name: "Tullot",
+    email: "stullot0@seesaa.net",
+    telephone: "725-381-5215",
+    dateDeNaissance: "11/16/2021",
+  },
+  {
+    id: 2,
+    first_name: "Ingrim",
+    last_name: "Linstead",
+    email: "ilinstead1@histats.com",
+    telephone: "959-807-0733",
+    dateDeNaissance: "11/29/2020",
+  },
+  {
+    id: 3,
+    first_name: "Stace",
+    last_name: "Keene",
+    email: "skeene2@wired.com",
+    telephone: "716-587-0288",
+    dateDeNaissance: "8/27/2023",
+  },
+  {
+    id: 4,
+    first_name: "Marcela",
+    last_name: "Tamlett",
+    email: "mtamlett3@wisc.edu",
+    telephone: "520-864-7866",
+    dateDeNaissance: "5/31/2019",
+  },
+  {
+    id: 5,
+    first_name: "Elton",
+    last_name: "Pfeffle",
+    email: "epfeffle4@acquirethisname.com",
+    telephone: "889-685-2619",
+    dateDeNaissance: "7/23/2017",
+  },
+];
+module.exports = utilisateurs;
--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const users = require("./data");
 
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
 // app.get("/", (req, res) => {
 //   // res.status(200).send(" test serveur");
 //   res.sendFile(path.resolve(__dirname, "./views/listDeContactPage.html"));
 // });
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("home", { users });
+});
+app.get("/user/:userId", (req, res) => {
+  const { userId } = req.params;
+  const findUser = users.find((u) => u.id === +userId);
+  if (!findUser) return res.status(404).send("product doesn not exist");
+  res.json(findUser);
+});
 
 app.listen(port, () => {
   console.log(`Serveur is on  http://localhost:${port}`);
