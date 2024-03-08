4a8baa664ab9492a819cc9be78092f9d08de7cc5
afker please <afker2020@gmail.com>
2024-03-08 08:59:26 +0100

    creating de chemin 404 + users page

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send(" test serveur");
-});
+app.use(express.static("./public"));
+// app.get("/", (req, res) => {
+//   // res.status(200).send(" test serveur");
+//   res.sendFile(path.resolve(__dirname, "./views/listDeContactPage.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Serveur is on  http://localhost:${port}`);
+});
+
+// error typo de url
+app.all("*", (req, res) => {
+  res.status(404).send(` <h1>Ressource not found</h1> `);
 });
